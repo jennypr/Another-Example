fa42c860b98eaf9ddd904b75cdbbb25c6a64b3c0
jennyporja <porjajenny@gmail.com>
2025-01-22 11:04:06 -0500

    Lesson001

---
+++ src/com/neotech/lesson001/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.neotech.lesson001;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		System.out.println("Hello Beautiful World");
+
+	}
+
+}
---
+++ src/com/neotech/lesson001/TestMain.java
@@ -0,0 +1,16 @@
+package com.neotech.lesson001;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// let's display connected lines / delete the 'ln' from print
+		
+		System.out.print("Let's test displaying two connected lines");// Don't jump to a new line after displaying the line
+System.out.print(". This is the second line!!");
+System.out.println(".End of text.");
+System.out.println("Hi");
+
+//print will display without jumping to a new line.
+	}
+
+}
